af07bef8f83efbe0b3d2b0eab147e2ca26cb129e
Neillcllghn <neillcllghn@outlook.com>
2023-03-16 19:34:29 +0000

    Add

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -6,3 +6,19 @@ from taskmanager.models import Category, Task  # to import from models
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+        # to match the import above
+    return render_template("add_category.html")
